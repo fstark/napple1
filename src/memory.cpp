3c595d2f7cd0798bae4565e383424fc64b300c34
Frederic Stark <frederic.stark@gmail.com>
2025-04-26 14:39:38 +0200

    Completed rom512 implementation

--- src/memory.cpp
+++ src/memory.cpp
@@ -21,10 +21,10 @@
  */
 /*
  * Memory map of napple1
- * 
+ *
  * - 4K in low memory ----------------------------------------------------
  * $0000 - $00FF   RAM (zero page)
- * $0100 - $01FF   RAM (stack) 
+ * $0100 - $01FF   RAM (stack)
  * $0200 - $027F   Input Buffer used by monitor
  * $0280 - $0FFF   RAM
  * - Hardware access -----------------------------------------------------
@@ -42,18 +42,19 @@
  * -----------------------------------------------------------------------
  * $FF00        WOZMON Monitor
  * $FFEF		Echo
- * $FFFF	
- * ----------------------------------------------------------------------- 
+ * $FFFF
+ * -----------------------------------------------------------------------
  * 8K machine has RAM from $0000 to $1FFF
  * 4K+4K machine has RAM from $0000 to $0FFF and $E000 to $EFFF
  * 32K machine has RAM from $0000 to $7FFF
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 #include "pia6820.h"
 #include "memory.h"
@@ -69,52 +70,51 @@ static unsigned char mem[65536];
 
 int rom512kpresent = 0;
 
+eMemType s_memType[256]; /* Type of all memory pages */
 
-eMemType s_memType[256];	/* Type of all memory pages */
-
-void memFill( uint8_t page, uint8_t value )
+void memFill(uint8_t page, uint8_t value)
 {
     int i;
     for (i = 0; i < 256; i++)
     {
-        mem[page*256+i] = value;
+        mem[page * 256 + i] = value;
     }
 }
 
-void setMemType( unsigned char page, eMemType type )
+void setMemType(unsigned char page, eMemType type)
 {
     unsigned char v = 0xaa;
     switch (type)
     {
-        case MEM_RAM:
-            v = 0x00;
-            break;
-        case MEM_ROM:
-            v = 0xff;
-            break;
-        case MEM_UNALOCATED:
-            v = page;	/* Apple1 unallocated space returns MSB */
-            break;
+    case MEM_RAM:
+        v = 0x00;
+        break;
+    case MEM_ROM:
+        v = 0xff;
+        break;
+    case MEM_UNALOCATED:
+        v = page; /* Apple1 unallocated space returns MSB */
+        break;
     }
 
     s_memType[page] = type;
-    memFill( page, v );
+    memFill(page, v);
 }
 
-void fillMemType( unsigned char start, int len, eMemType type )
+void fillMemType(unsigned char start, int len, eMemType type)
 {
     int i;
 
     for (i = 0; i < len; i++)
-        setMemType( start+i, type );
+        setMemType(start + i, type);
 }
 
 void resetMemType()
 {
-    fillMemType( 0, 256, MEM_UNALOCATED );
+    fillMemType(0, 256, MEM_UNALOCATED);
 }
 
-int loadRom( unsigned char startPage, const char *romfile, uint16_t *startAdrs, uint16_t *endAdrs )
+int loadRom(unsigned char startPage, const char *romfile, uint16_t *startAdrs, uint16_t *endAdrs)
 {
     int start = 0;
     int len = 0;
@@ -123,14 +123,15 @@ int loadRom( unsigned char startPage, const char *romfile, uint16_t *startAdrs, 
         start = *startAdrs;
 
     if (endAdrs)
-        len = *endAdrs-start;
+        len = *endAdrs - start;
 
     // printf( "loadRom: %s $%02x %ld %ld\n", romfile, startPage, start, len );
     FILE *fd = fopen(romfile, "rb");
     int i;
 
-    if (!fd) {
-        console_printf( "Failed to open %s\n", romfile);
+    if (!fd)
+    {
+        console_printf("Failed to open %s\n", romfile);
         perror("fopen");
         return -1;
     }
@@ -138,7 +139,8 @@ int loadRom( unsigned char startPage, const char *romfile, uint16_t *startAdrs, 
     if (!len)
     {
         fseek(fd, 0, SEEK_END);
-        if ((len=ftell(fd))==-1) {
+        if ((len = ftell(fd)) == -1)
+        {
             fprintf(stderr, "Failed to size: %s\n", romfile);
             fclose(fd);
             return -1;
@@ -147,7 +149,7 @@ int loadRom( unsigned char startPage, const char *romfile, uint16_t *startAdrs, 
 
     fseek(fd, start, SEEK_SET);
 
-    if ((len%256)!=0)
+    if ((len % 256) != 0)
     {
         fprintf(stderr, "ROM size is not multiple of 256: %s\n", romfile);
         fclose(fd);
@@ -155,38 +157,38 @@ int loadRom( unsigned char startPage, const char *romfile, uint16_t *startAdrs, 
     }
 
     /* Read each page of the rom file */
-    for (i = startPage; i < startPage + len/256; i++)
+    for (i = startPage; i < startPage + len / 256; i++)
     {
-        if (i*256>0xFFFF)
+        if (i * 256 > 0xFFFF)
         {
             fprintf(stderr, "ROM file too large: %s\n", romfile);
             fclose(fd);
             return -1;
         }
-        fillMemType( i, 1, MEM_ROM );
-        fread(&mem[i*256], 1, 256, fd);
+        fillMemType(i, 1, MEM_ROM);
+        fread(&mem[i * 256], 1, 256, fd);
     }
 
     fclose(fd);
 
-        //	Not sure this makes any sense
+    //	Not sure this makes any sense
     if (startAdrs)
         *startAdrs = start;
 
     if (endAdrs)
-        *endAdrs = start+len;
+        *endAdrs = start + len;
 
     return 0;
 }
 
 static FILE *trace_file;
 
-void ansi_red() { printf( "\033[31m" ); }
-void ansi_green() { printf( "\033[32m" ); }
-void ansi_black() { printf( "\033[0m" ); }
+void ansi_red() { printf("\033[31m"); }
+void ansi_green() { printf("\033[32m"); }
+void ansi_black() { printf("\033[0m"); }
 
 // a variant of fprintf that prints in the file "trace_file"
-int trace_printf( const char *format, ... )
+int trace_printf(const char *format, ...)
 {
     va_list args;
     int res;
@@ -194,28 +196,28 @@ int trace_printf( const char *format, ... )
     if (!trace_file)
     {
         // open and truncate the file
-        trace_file = fopen( "trace.txt", "w" );
+        trace_file = fopen("trace.txt", "w");
     }
 
-    va_start( args, format );
-    res = vfprintf( trace_file, format, args );
-    va_end( args );
+    va_start(args, format);
+    res = vfprintf(trace_file, format, args);
+    va_end(args);
 
-    fflush( trace_file );
+    fflush(trace_file);
 
     return res;
 }
 
-int console_printf( const char *format, ... )
+int console_printf(const char *format, ...)
 {
     va_list args;
     int res;
 
     ansi_green();
 
-    va_start( args, format );
-    res = vfprintf( stdout, format, args );
-    va_end( args );
+    va_start(args, format);
+    res = vfprintf(stdout, format, args);
+    va_end(args);
 
     ansi_black();
 
@@ -227,24 +229,25 @@ int console_printf( const char *format, ... )
 // print thread id
 void trace_tid()
 {
-    trace_printf( "Thread id: %d\n", pthread_self() );
+    trace_printf("Thread id: %d\n", pthread_self());
 }
 
-void dumpMem( void )
+void dumpMem(void)
 {
     //	 Loop over all memory pages and print the type
-    for (int i=0;i!=256;i++)
+    for (int i = 0; i != 256; i++)
     {
-        console_printf( "%02x: %s ", i, s_memType[i]==MEM_RAM ? "RAM" : s_memType[i]==MEM_ROM ? "ROM" : "UNALLOCATED" );
+        console_printf("%02x: %s ", i, s_memType[i] == MEM_RAM ? "RAM" : s_memType[i] == MEM_ROM ? "ROM"
+                                                                                                 : "UNALLOCATED");
 
         // print content of page in hex, address followed by 16 bytes per line
-        if (s_memType[i]==MEM_RAM || s_memType[i]==MEM_ROM)
+        if (s_memType[i] == MEM_RAM || s_memType[i] == MEM_ROM)
         {
             // Check if the 256 bytes are not all the same
             int same = 1;
-            for (int j=1;j!=256;j++)
+            for (int j = 1; j != 256; j++)
             {
-                if (mem[i*256]!=mem[i*256+j])
+                if (mem[i * 256] != mem[i * 256 + j])
                 {
                     same = 0;
                     break;
@@ -253,87 +256,99 @@ void dumpMem( void )
 
             if (same)
             {
-                console_printf( "%02x * 256\n", mem[i*256] );
+                console_printf("%02x * 256\n", mem[i * 256]);
             }
             else
             {
-                console_printf( "\n" );
-                for (int j=0;j!=256;j+=16)
+                console_printf("\n");
+                for (int j = 0; j != 256; j += 16)
                 {
-                    console_printf( "%04x: ", i*256+j );
-                    for (int k=0;k!=16;k++)
+                    console_printf("%04x: ", i * 256 + j);
+                    for (int k = 0; k != 16; k++)
                     {
-                        console_printf( "%02x ", mem[i*256+j+k] );
+                        if (k == 8)
+                            console_printf(" ");
+                        console_printf("%02x ", mem[i * 256 + j + k]);
                     }
-                    console_printf( "\n" );
+                    console_printf("    ");
+                    for (int k = 0; k != 16; k++)
+                    {
+                        if (k == 8)
+                            console_printf(" ");
+
+                        auto c = mem[i * 256 + j + k];
+                        console_printf("%c", isgraph(c) ? c : '.');
+                    }
+                    console_printf("\n");
                 }
             }
         }
         else
         {
-            console_printf( "\n" );
+            console_printf("\n");
         }
     }
 }
 
-int loadRom32Page( const char *rom32, int page4k )
+int loadRom32Page(const char *rom32, int page4k)
 {
     int offset = page4k;
-    if (offset>=32768)
+    if (offset >= 32768)
         offset -= 32768;
-    trace_printf( "loadRom32Page: %s $%04x %d\n", rom32, page4k, offset );
+    trace_printf("loadRom32Page: %s $%04x %d\n", rom32, page4k, offset);
     uint16_t start = offset;
-    uint16_t end = start+4096;
-    return loadRom( page4k/256, rom32, &start, &end );
+    uint16_t end = start + 4096;
+    return loadRom(page4k / 256, rom32, &start, &end);
 }
 
 /* Loads a 32K rom according to a map configuration */
-int loadRom32( const char *rom32, uint16_t config )
+int loadRom32(const char *rom32, uint16_t config)
 {
-    for (int i=0;i!=16;i++)
+    for (int i = 0; i != 16; i++)
     {
-        if (config & (1<<i))
-            if (loadRom32Page( rom32, i*4096 ))
+        if (config & (1 << i))
+            if (loadRom32Page(rom32, i * 4096))
                 return -1;
     }
 
     return 0;
 }
 
-uint8_t *getMemoryPtr( uint16_t address )
+uint8_t *getMemoryPtr(uint16_t address)
 {
-    return mem+address;
+    return mem + address;
 }
 
-void bank512( uint16_t adrs )
+void bank512(uint16_t adrs)
 {
-    if (adrs>=0xa000 && adrs<0xc000)
+    if (adrs >= 0xa000 && adrs < 0xc000)
     {
-        int bank = adrs&0xf;
-        copyBank512( bank, mem );
+        int bank = adrs & 0xf;
+        copyBank512(bank, mem);
     }
 }
 
-unsigned char memPeek( unsigned short address )
+unsigned char memPeek(unsigned short address)
 {
-    bank512( address );
+    bank512(address);
     return mem[address];
 }
 
 unsigned char memRead(unsigned short address)
 {
-    bank512( address );
+    bank512(address);
 
     if (address == 0xD010)
     {
         unsigned char c = readKbd();
         // console_printf( "KEYBOARD: READ %02x '%c'\n", c, c&0x7f );
-        writeKbdCr( 0x27 );
-// 		if (!(readKbdCr()&0x80))
-// \			console_printf( "KEYBOARD: cleared\n" );
+        writeKbdCr(0x27);
+        // 		if (!(readKbdCr()&0x80))
+        // \			console_printf( "KEYBOARD: cleared\n" );
         return c;
     }
-    if (address == 0xD011) {
+    if (address == 0xD011)
+    {
         unsigned char v = readKbdCr();
         // if (v&0x80)
         // 	console_printf( "KEYBOARD: key pressed\n" );
@@ -354,17 +369,17 @@ unsigned char memRead(unsigned short address)
 
 int isRam(unsigned short address)
 {
-    return s_memType[address/256] == MEM_RAM;
+    return s_memType[address / 256] == MEM_RAM;
 }
 
 int isRom(unsigned short address)
 {
-    return s_memType[address/256] == MEM_ROM;
+    return s_memType[address / 256] == MEM_ROM;
 }
 
 void memWrite(unsigned short address, unsigned char value)
 {
-    bank512( address );
+    bank512(address);
 
     if (isRam(address))
         mem[address] = value;
@@ -377,16 +392,19 @@ void memWrite(unsigned short address, unsigned char value)
     return;
 }
 
-void dumpCore( const char *filename )
+void dumpCore(const char *filename)
 {
     int i;
     FILE *fd;
-    char input[MSG_LEN_MAX +1];
-    char corename[5 + MSG_LEN_MAX +1]; /* 'core/' + input string */
+    char input[MSG_LEN_MAX + 1];
+    char corename[5 + MSG_LEN_MAX + 1]; /* 'core/' + input string */
 
-    if (filename) {
+    if (filename)
+    {
         strcpy(input, filename);
-    } else {
+    }
+    else
+    {
         gets_msgbuf("Dump core. Filename: ", input);
     }
 
@@ -403,8 +421,8 @@ void dumpCore( const char *filename )
 int loadCore(void)
 {
     FILE *fd;
-    char input[MSG_LEN_MAX +1];
-    char corename[5 + MSG_LEN_MAX +1]; /* 'core/' + input string */
+    char input[MSG_LEN_MAX + 1];
+    char corename[5 + MSG_LEN_MAX + 1]; /* 'core/' + input string */
     size_t s = 0;
     unsigned char buf[65536];
     int i;
@@ -413,13 +431,17 @@ int loadCore(void)
     snprintf(corename, sizeof(corename), "core/%s", input);
 
     fd = fopen(corename, "r");
-    if (fd) {
-        s = fread(&buf[0], 1, MEMMAX+1, fd);
+    if (fd)
+    {
+        s = fread(&buf[0], 1, MEMMAX + 1, fd);
         fclose(fd);
     }
-    if (s) { 
+    if (s)
+    {
         gets_msgbuf("Load core completed: ", input);
-    } else {
+    }
+    else
+    {
         gets_msgbuf("Failed to open core file: ", input);
         return 0;
     }
--- src/rom512.cpp
+++ src/rom512.cpp
@@ -5,35 +5,35 @@
 
 static uint8_t *rom512 = NULL;
 
-int loadRom512( const char *filename )
+int loadRom512(const char *filename)
 {
     if (rom512)
         releaseRom512();
-    rom512  = (uint8_t *)malloc(512*1024);
+    rom512 = (uint8_t *)malloc(512 * 1024);
     if (!rom512)
         return -1;
-    
-    FILE *fd = fopen( filename, "rb" );
+
+    FILE *fd = fopen(filename, "rb");
     if (!fd)
     {
         releaseRom512();
         return -1;
     }
-    
-    if (fread( rom512, 512*1024, 1, fd ) != 1)
+
+    if (fread(rom512, 512 * 1024, 1, fd) != 1)
     {
         releaseRom512();
-        fclose( fd );
+        fclose(fd);
         return -1;
     }
 
-    fclose( fd );
+    fclose(fd);
 
     return 0;
 }
 
 //  Releases the loaded ROM
-void releaseRom512( void )
+void releaseRom512(void)
 {
     if (rom512)
     {
@@ -43,17 +43,17 @@ void releaseRom512( void )
 }
 
 //  Copy a bank of the ROM in memory (pass a pointer to a 64K block)
-int copyBank512( uint8_t bank, uint8_t *memory )
+int copyBank512(uint8_t bank, uint8_t *memory)
 {
     if (!rom512)
         return -1;
 
-    u_int8_t *src = rom512 + bank * 32*1024;
-        //  Copy range 2000-7fff
-    memcpy( memory+0x2000, src+0x2000, 24*1024 );
-        //  Copy range 0000-1fff
-    memcpy( memory+0x8000, src, 8*1024 );
-        //  Duplicate 6000-6fff
-    memcpy( memory+0x6000, src+0xe000, 4*1024 );
+    u_int8_t *src = rom512 + bank * 32 * 1024;
+    //  Copy range 2000-7fff
+    memcpy(memory + 0x2000, src + 0x2000, 24 * 1024);
+    //  Copy range 0000-1fff
+    memcpy(memory + 0x8000, src, 8 * 1024);
+    //  Duplicate 6000-6fff
+    memcpy(memory + 0xe000, src + 0x6000, 4 * 1024);
     return 0;
 }
